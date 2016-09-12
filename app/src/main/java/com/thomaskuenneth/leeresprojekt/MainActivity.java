dd0dce1dd8230ed43e5baf640b275385055edc29
Administrator <f.neumann.berlin@googlemail.com>
2016-09-12 10:41:26 +0200

    first

--- app/src/main/java/com/thomaskuenneth/leeresprojekt/MainActivity.java
+++ app/src/main/java/com/thomaskuenneth/leeresprojekt/MainActivity.java
@@ -3,13 +3,15 @@ package com.thomaskuenneth.leeresprojekt;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
-
+    static EditText name;
+    static EditText phone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,15 +24,27 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), Main2Activity.class);
-                EditText name = (EditText) findViewById(R.id.name);
-                EditText phone = (EditText) findViewById(R.id.phone) ;
+                name = (EditText) findViewById(R.id.name);
+                phone = (EditText) findViewById(R.id.phone) ;
                 intent.putExtra("name", name.getText().toString());
-                intent.putExtra("phone",phone.getText().toString());
+                intent.putExtra("phone", phone.getText().toString());
                 startActivity(intent);
             }
         });
 
+        Button insert = (Button)findViewById(R.id.insert);
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_INSERT, ContactsContract.Contacts.CONTENT_URI);
+                intent.putExtra("finshActivitiOnSaveCompleted",true);
+                intent.putExtra(ContactsContract.Intents.Insert.NAME,name.getText());
+                intent.putExtra(ContactsContract.Intents.Insert.PHONE,phone.getText());
+                intent.putExtra(ContactsContract.Intents.Insert.PHONE_TYPE,ContactsContract.CommonDataKinds.Phone.TYPE_HOME);
+                startActivity(intent);
 
+            }
+        });
 
 
     }
