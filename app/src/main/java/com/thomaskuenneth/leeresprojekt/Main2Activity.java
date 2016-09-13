a1df305e99755371b731b130a512eb5f335b4e6c
Administrator <f.neumann.berlin@googlemail.com>
2016-09-13 08:22:48 +0200

    bilder

--- app/src/main/java/com/thomaskuenneth/leeresprojekt/Main2Activity.java
+++ app/src/main/java/com/thomaskuenneth/leeresprojekt/Main2Activity.java
@@ -1,11 +1,17 @@
 package com.thomaskuenneth.leeresprojekt;
 
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.view.View;
 import android.widget.TextView;
 
 public class Main2Activity extends Activity {
+
+    private static final int NOTIFICATION_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +24,11 @@ public class Main2Activity extends Activity {
             view.setText(intent.getStringExtra("name") + "\n" + intent.getStringExtra("phone"));
         }
 
+    }
+    public void sendNotification(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://developer.android.com/references/android/app/Notification.html"));
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,0);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.drawable.ic_stat_notification);
     }
 }
